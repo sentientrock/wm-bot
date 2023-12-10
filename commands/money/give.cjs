c5d1c6c3bcf06adca397fea9c72577159d7cd5c3
azeezh <amaze1013@gmail.com>
2023-12-10 16:38:56 -0500

    added give command

--- commands/money/give.cjs
+++ commands/money/give.cjs
@@ -3,31 +3,31 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('spend')
-        .setDescription('Spend money on something!')
-        .addIntegerOption(option => 
+        .setName('give')
+        .setDescription('Give money to another player!')
+        .addUserOption(option => 
+            option
+                .setName('player')
+                .setDescription('The player to give money to')
+                .setRequired(true)
+        )
+        .addIntegerOption(option =>
             option
                 .setName('amount')
-                .setDescription('Amount of money to spend')
-                .setRequired(true))
-        .addStringOption(option => 
-            option
-                .setName('message')
-                .setDescription('Description of what you spent money on')
-                .setRequired(true)
+                .setDescription('Amount to give')
+                .setRequired(true)   
         ),
         async execute(interaction, jwt) {
             const infoDoc = new GoogleSpreadsheet('1BrPCgisbosG1TDDctmq4iS3gzTfFW8HzGzoefnD4AzE', jwt);
             await infoDoc.loadInfo();
             const moneySheet = infoDoc.sheetsByIndex[3];
             const number = interaction.options.getInteger('amount');
+            const player = interaction.options.getUser('player');
 
             const today = new Date();
             const dd = String(today.getDate()).padStart(2, '0');
             const mm = String(today.getMonth() + 1).padStart(2, '0');
             const currDate = mm + '/' + dd;
-
-            const moneyMessage = interaction.options.getString('message');
 
             if (number === null) {
                 await interaction.reply({ content: `Please enter an integer amount when spending money :)`});
@@ -38,8 +38,11 @@ module.exports = {
                 await interaction.reply({ content: `Please enter a value greater than 0!`});
                 return;
             }
-            
-            await moneySheet.addRow({Disc: `${interaction.user.username}`, Transaction: `-${number}`, Note: `${moneyMessage}`, Date: `${currDate}`});
-            await interaction.reply({ content: `You successfully spent ${number} gold!`});
+
+            await moneySheet.addRow({Disc: `${interaction.user.username}`, Transaction: `-${number}`, Note: `${interaction.user.username} to ${player.username}`, Date: `${currDate}`});
+            await moneySheet.addRow({Disc: `${player.username}`, Transaction: `${number}`, Note: `${interaction.user.username} to ${player.username}`, Date: `${currDate}`});
+
+            await interaction.reply({ content: `You gave ${number} gold to ${player.username}!`})
+
         }
 }
